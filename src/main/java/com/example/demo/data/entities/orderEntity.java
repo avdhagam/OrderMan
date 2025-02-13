065860eb08b278c0acae4cd37a4d069159b6a066
avdhagam <davanisri@gmail.com>
2025-02-13 11:58:40 +0530

    added files

---
+++ src/main/java/com/example/demo/data/entities/orderEntity.java
@@ -0,0 +1,29 @@
+package com.example.demo.data.entities;
+
+import com.example.demo.data.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Table(name = "orders")
+@Entity
+@Data
+public class orderEntity {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal totalamount;
+}
---
+++ src/main/java/com/example/demo/data/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.example.demo.data.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
